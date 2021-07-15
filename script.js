da1ed2b018e8b9915557fbd45f0dcde2b1d81e53
Afaq Ali <afaqali2010@gmail.com>
2021-07-15 19:47:13 -0400

    changed to aync await

--- script.js
+++ script.js
@@ -2,28 +2,21 @@ const img = document.querySelector('img');
 const btn = document.querySelector('.submit-container');
 const search = document.querySelector('input');
 
-
-
-function getGIF() {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=FPV2YZmDl68tTOF617F6usbRavLumZN9&s=${search.value}`, {mode: 'cors'})
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        //console.log(response.data.images.original.url)
-        img.src = response.data.images.original.url;
-    })
-    .catch(err => {
+async function getGIF() {
+    try {
+        let response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=FPV2YZmDl68tTOF617F6usbRavLumZN9&s=${search.value}`, {mode: 'cors'})
+        let parsedResponse = await response.json();
+        img.src = parsedResponse.data.images.original.url; 
+    }
+    catch (err) {
         console.log(err);
         img.src = 'https://media.giphy.com/media/9J7tdYltWyXIY/giphy.gif';
-    })
+    }
 }
-
 
 btn.addEventListener('click', () => {
     getGIF();
 })
-
 
 document.addEventListener("keyup", function(e) {
 
